ba93685f0a501d7d916637f8e236b6760aa7b86c
cduarte <duarte.camd@gmail.com>
2022-12-10 18:58:39 -0300

    Agrego código de monedas.

---
+++ src/main/java/ar/com/camd/converter/model/CurrencyCode.java
@@ -0,0 +1,17 @@
+/**
+ * CurrencyCode.java
+ * 
+ * @copyright 2022, Christian Ariel Modesto Duarte. All rights reserved.
+ */
+package ar.com.camd.converter.model;
+
+/**
+ * <code>CurrencyCode</code>
+ * Provides currency codes.
+ * 
+ * @author    Christian Ariel Modesto Duarte <duarte.camd@gmail.com>
+ * @version  1.0.0-SNAPSHOT
+ */
+public enum CurrencyCode {
+	ARS, USD, EUR, JPY, GBP, KRW
+}
