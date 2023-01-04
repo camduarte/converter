4d954aabff920eb21c521d6bb644cdb76a74b9d4
cduarte <duarte.camd@gmail.com>
2023-01-03 23:13:42 -0300

    Add empty controller, fxml for menu and css styles.

---
+++ src/main/java/ar/com/camd/converter/controller/ButtonOKController.java
@@ -0,0 +1,17 @@
+/**
+ * ButtonOKController.java
+ * 
+ * @copyright 2023, Christian Ariel Modesto Duarte. All rights reserved.
+ */
+package ar.com.camd.converter.controller;
+
+/**
+ * <code>ButtonOKController</code>
+ * Controls the button actions.
+ * 
+ * @author	Christian Ariel Modesto Duarte <duarte.camd@gmail.com>
+ * @version  1.0.0-SNAPSHOT
+ */
+public class ButtonOKController {
+	
+}
