ca54776a816e597f26c1e92be100546a91b401a1
cduarte <duarte.camd@gmail.com>
2023-01-05 04:05:31 -0300

    Add temperature model.

---
+++ src/main/java/ar/com/camd/converter/model/Temperature.java
@@ -0,0 +1,43 @@
+/**
+ * Temperature.java
+ * 
+ * @copyright 2023, Christian Ariel Modesto Duarte. All rights reserved.
+ */
+package ar.com.camd.converter.model;
+
+/**
+ * <code>Temperature</code>
+ * Represents the temperature.
+ * 
+ * @author	Christian Ariel Modesto Duarte <duarte.camd@gmail.com>
+ * @version	1.0.0-SNAPSHOT
+ */
+public class Temperature {
+
+	private TemperatureUnit unit;
+	private Double value;
+
+	/**
+	 * @param unit The temperature unit.
+	 * @param value The temperature value.
+	 */
+	public Temperature(TemperatureUnit unit, Double value) {
+		this.unit = unit;
+		this.value = value;
+	}
+
+	/**
+	 * @return the temperature unit.
+	 */
+	public TemperatureUnit getUnit() {
+		return unit;
+	}
+
+	/**
+	 * @return the temperature value.
+	 */
+	public Double getValue() {
+		return value;
+	}
+
+}
